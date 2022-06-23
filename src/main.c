ef1c554646d75c24c966882d84b67d0726049ea3
Olly Swanson <olly.swanson95@gmail.com>
2022-06-23 19:15:45 +0100

    Disassemble `Chunk`

--- src/main.c
+++ src/main.c
@@ -1,9 +1,13 @@
 #include "chunk.h"
 #include "common.h"
+#include "debug.h"
 
 int main(int argc, const char *argv[]) {
   Chunk chunk;
   init_chunk(&chunk);
   write_chunk(&chunk, OP_RETURN);
+
+  disassemble_chunk(&chunk, "test chunk");
+
   free_chunk(&chunk);
 }
